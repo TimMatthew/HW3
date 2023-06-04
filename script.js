ac54ffc39f05dccd99338f3d6544c478e4fa42f6
TimMatthew <t.mattw.0000@gmail.com>
2023-06-04 12:59:10 +0300

    removing goods done

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-window.onload = addGood;
+window.addEventListener('load', addGood);
+window.addEventListener('load', function() {
+    var goodsPanels = document.querySelectorAll('.goods-panel');
+    goodsPanels.forEach(function(goodsPanel) {
+        var boughtButton = goodsPanel.querySelector('.bought');
+        boughtButton.addEventListener('click', function() {
+            togglePurchase(goodsPanel);
+        });
+    });
+});
+window.addEventListener(`load`, function(){
+    var goodsPanels = document.querySelectorAll('.goods-panel');
+    goodsPanels.forEach(function(goodsPanel) {
+        var removeButton = goodsPanel.querySelector('.cancel');
+        removeButton.addEventListener('click', function() {
+            goodsPanel.remove();
+        });
+    });
+});
 
-function addGood(){
+function addGood() {
     var addButton = document.getElementById('add_button');
     var searchBar = document.getElementById('search_bar');
     var goodsContainer = document.querySelector('.goods');
@@ -9,16 +27,128 @@ function addGood(){
         var productName = searchBar.value.trim();
 
         if (productName !== '') {
-            var existingGoodsPanel = document.querySelector('.goods-panel');
-            var newGoodsPanel = existingGoodsPanel.cloneNode(true);
+            var newGoodsPanel = createGoodsPanel();
 
             var leftSpan = newGoodsPanel.querySelector('.left');
             leftSpan.textContent = productName;
+
+            // takes 2 times to clicke for toggling
+            var boughtButton = newGoodsPanel.querySelector('.bought');
+            boughtButton.addEventListener('click', function() {
+                var goodsPanels = document.querySelectorAll('.goods-panel');
+                goodsPanels.forEach(function(goodsPanel) {
+                var boughtButton = goodsPanel.querySelector('.bought');
+                 boughtButton.addEventListener('click', function() {
+                togglePurchase(goodsPanel);
+        });
+    });
+            });
+
+            var removeButton = newGoodsPanel.querySelector('.cancel');
+            removeButton.addEventListener('click', function() {
+            removeGood(newGoodsPanel);
+            });
 
             goodsContainer.appendChild(newGoodsPanel);
 
             searchBar.value = '';
             searchBar.focus();
         }
-    });   
+    });
+}
+
+function togglePurchaseForExistingGoods() {
+    var existingGoods = document.querySelectorAll('.goods-panel');
+    
+    existingGoods.forEach(function(goodsPanel) {
+        var boughtButton = goodsPanel.querySelector('.bought');
+        boughtButton.addEventListener('click', function() {
+            togglePurchase(goodsPanel);
+        });
+    });
+}
+
+function togglePurchase(goodsPanel) {
+    var statucBar = goodsPanel.querySelector(`.status`);
+    var leftDiv = goodsPanel.querySelector('.left');
+    var centerDiv = goodsPanel.querySelector('.center');
+    var deleteButton = goodsPanel.querySelector('.cancel');
+    var boughtButton = goodsPanel.querySelector('.bought');
+
+    if (boughtButton.textContent == 'Купити товар') {
+        centerDiv.hidden = true;
+        deleteButton.hidden = true;
+        statucBar.textContent = `Куплено`;
+        leftDiv.classList.add('text-line-through');
+        boughtButton.textContent = 'Скасувати покупку';
+    } else {
+        centerDiv.hidden = false;
+        deleteButton.hidden = false;
+        statucBar.textContent = `Не куплено`;
+        leftDiv.classList.remove('text-line-through');
+        boughtButton.textContent = 'Купити товар';
+    }
+}
+
+function createGoodsPanel() {
+    var goodsPanel = document.createElement('section');
+    goodsPanel.classList.add('goods-panel');
+    
+    var leftSpan = document.createElement('span');
+    leftSpan.classList.add('left');
+    leftSpan.textContent = 'Печиво';
+    goodsPanel.appendChild(leftSpan);
+
+    var centerDiv = document.createElement('div');
+    centerDiv.classList.add('center');
+
+    var minusButton = document.createElement('button');
+    minusButton.classList.add('minus');
+    minusButton.textContent = '-';
+    minusButton.setAttribute('data-tooltip', 'Зменшити к-сть продуктів');
+    centerDiv.appendChild(minusButton);
+
+    var counterSpan = document.createElement('span');
+    counterSpan.classList.add('counter');
+    counterSpan.textContent = '1';
+    centerDiv.appendChild(counterSpan);
+
+    var plusButton = document.createElement('button');
+    plusButton.classList.add('plus');
+    plusButton.textContent = '+';
+    plusButton.setAttribute('data-tooltip', 'Збільшити к-сть продуктів');
+    centerDiv.appendChild(plusButton);
+
+    goodsPanel.appendChild(centerDiv);
+
+    var rightDiv = document.createElement('div');
+    rightDiv.classList.add('right');
+
+    var statusP = document.createElement('p');
+    statusP.classList.add('status');
+    statusP.textContent = 'Не куплено';
+    statusP.setAttribute('data-tooltip', "Товар занесено в категорію 'Куплено'");
+    rightDiv.appendChild(statusP);
+
+    var boughtButton = document.createElement('button');
+    boughtButton.classList.add('bought');
+    boughtButton.textContent = 'Купити товар';
+    boughtButton.setAttribute('data-tooltip', "Товар занесено в категорію 'Залишилося'");
+    boughtButton.addEventListener('click', function() {
+        togglePurchase(goodsPanel);
+    });
+    rightDiv.appendChild(boughtButton);
+
+    var cancelButton = document.createElement('button');
+    cancelButton.classList.add('cancel');
+    cancelButton.textContent = 'Х';
+    cancelButton.setAttribute('data-tooltip', "Скасувати дії");
+    cancelButton.addEventListener('click', function() {
+        goodsPanel.remove();
+    });
+    rightDiv.appendChild(cancelButton);
+
+    goodsPanel.appendChild(rightDiv);
+
+    return goodsPanel;
 }
